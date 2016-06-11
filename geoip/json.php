f053e1e097650e0804e1b76084b6092fbaf6c105
Thrickr Gwo <ze3kr@icloud.com>
2016-06-11 11:24:30 +0800

    no message

--- geoip/json.php
+++ geoip/json.php
@@ -4,26 +4,38 @@ header('Content-Type: application/json');
 if( !isset( $_GET['host'] ) ){
 	$_GET['host'] = $_SERVER['REMOTE_ADDR'];
 }
-if( !filter_var($_GET['host'], FILTER_VALIDATE_IP) ){
+
+$error_message = '';
+
+if( filter_var($_GET['host'], FILTER_VALIDATE_IP) ){
+	$type = 'ip';
+	$asn = geoip_asnum_by_name( $_GET['host'] );
+} else {
 	$_GET['host'] = gethostbyname( $_GET['host'] );
 	$type = 'domain';
-} else {
-	$type = 'IP';
+	if( filter_var($_GET['host'], FILTER_VALIDATE_IP) ){
+		$asn = geoip_asnum_by_name( $_GET['host'] );
+	} else {
+		$asn = false;
+		$error_message = ' Cannot resolve host name.';
+		$_GET['host'] = '';
+	}
 }
-$asn = geoip_org_by_name( $_GET['host'] ); // I had rename `GeoLiteASNum.dat` to `GeoIPOrg.dat`, so `geoip_org_by_name` is used to get ASN.
-if( $geoip_data === false ) {
+if( $asn ) {
 	$data = [
-		'status' => 'error',
-		'description' => 'Address cannot be found in the database.',
+		'status' => 'success',
+		'description' => 'Address successfully found.',
 		'type' => $type,
+		'record' => geoip_record_by_name( $_GET['host'] ),
+		'domain' => geoip_domain_by_name( $_GET['host'] ),
+		'asn' => $asn,
 		'ip' => $_GET['host'],
 	];
 } else {
 	$data = [
-		'status' => 'success',
-		'description' => 'Address successfully found.',
-		'record' => geoip_record_by_name( $_GET['host'] ),
-		'domain' => geoip_domain_by_name( $_GET['host'] ),
+		'status' => 'error',
+		'description' => 'Address cannot be found in the database.'.$error_message,
+		'type' => $type,
 		'ip' => $_GET['host'],
 	];
 }
