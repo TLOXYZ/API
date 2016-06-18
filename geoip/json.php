b1425bcdc7215a2ef20a5aab932060e540e20478
Thrickr Gwo <ze3kr@icloud.com>
2016-06-18 19:37:59 +0800

    Add cache!

--- geoip/json.php
+++ geoip/json.php
@@ -8,15 +8,28 @@ if( !isset( $_GET['host'] ) ){
 $error_message = '';
 
 if( filter_var($_GET['host'], FILTER_VALIDATE_IP, FILTER_FLAG_IPV4) ){
+	$fetch = apcu_fetch($_GET['host']);
+	if($fetch){
+		exit( json_encode( $data ) );
+	}
 	$type = 'ipv4';
 	$asn = geoip_asnum_by_name( $_GET['host'] );
 	$domain = gethostbyaddr( $_GET['host'] );
 } elseif ( filter_var($_GET['host'], FILTER_VALIDATE_IP, FILTER_FLAG_IPV6) ) {
+	$fetch = apcu_fetch($_GET['host']);
+	if($fetch){
+		exit( json_encode( $fetch ) );
+	}
 	$type = 'ipv6';
 	$asn = false;
 	$error_message = ' Cannot use IPv6 at this time.';
 	$domain = gethostbyaddr( $_GET['host'] );
 } else {
+	$fetch = apcu_fetch($_GET['host']);
+	if($fetch){
+		$fetch = apcu_fetch($fetch);
+		exit( json_encode( $fetch ) );
+	}
 	$_GET['host'] = gethostbyname( $_GET['host'] );
 	$type = 'domain';
 	if( filter_var($_GET['host'], FILTER_VALIDATE_IP) ){
@@ -39,7 +52,8 @@ if( $asn ) {
 		'asn' => $asn,
 		'ip' => $_GET['host'],
 	];
-	
+	apcu_store($_GET['host'], $data, 3600);
+	apcu_store($domain, $_GET['host'], 3600);
 } else {
 	$data = [
 		'status' => 'error',
@@ -48,6 +62,7 @@ if( $asn ) {
 		'domain' => $domain,
 		'ip' => $_GET['host'],
 	];
+	header('Cache-Control: public, max-age=60');
 }
 
 echo json_encode( $data );
