cd55ec81f0b357f3ec405a2c0baecf34656b551a
Thrickr Gwo <ze3kr@icloud.com>
2016-07-15 17:28:41 +0800

    Add JSONP

---
+++ myip/jsonp.php
@@ -0,0 +1,16 @@
+<?php
+header('Cache-Control: no-cache');
+header('Content-type: application/json');
+require_once( '../geoipfix.php' );
+
+$data['country'] = $_SERVER['GEOIP_COUNTRY_CODE'];
+
+if( isset( $_SERVER["REAL_IP"] ) ){
+	$data['ip'] = $_SERVER["HTTP_CLIENT_IP"];// For the Beijing datacenter
+} elseif( isset( $_SERVER['HTTP_X_FORWARDED_FOR'] ) ){
+	$data['ip'] = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR'])[0]; // For our CDN, user can fake this header.
+} else {
+	$data['ip'] = $_SERVER['REMOTE_ADDR'];
+}
+
+echo $_GET['callback'],"([",json_encode($data),"])";
