33f7a9b0f55cd361ee665c2820a91116e2863825
Thrickr Gwo <ze3kr@icloud.com>
2016-07-17 10:29:11 +0800

    Add support for SAE

--- geoipfix.php
+++ geoipfix.php
@@ -2,6 +2,7 @@
 if($_SERVER["SERVER_ADMIN"] == 'saesupport@sina.cn'){
 	$_SERVER['GEOIP_COUNTRY_CODE'] = 'CN';
 	$_SERVER["REAL_IP"] = $_SERVER["HTTP_X_REAL_IP"];
+	header('X-TLO-Hostname: bj-server.tlo.xyz');
 } elseif( isset( $_SERVER['HTTP_CF_CONNECTING_IP'] ) ) {
 	$_SERVER["REAL_IP"] = $_SERVER['HTTP_CF_CONNECTING_IP'];
 	$_SERVER['GEOIP_COUNTRY_CODE'] = $_SERVER['HTTP_CF_IPCOUNTRY'];
